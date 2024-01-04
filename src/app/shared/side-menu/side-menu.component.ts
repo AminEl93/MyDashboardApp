72b275c6074bd6e7f3bfb0dfd2133f012bf03d2a
AminEl93 <amin_11_iverson@hotmail.com>
2024-01-04 17:44:37 +0100

    Menú lateral con las opciones

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '../../app.routes';
 
 @Component({
     selector: 'app-side-menu',
     standalone: true,
-    imports: [],
+    imports: [CommonModule, RouterModule],
     templateUrl: './side-menu.component.html'
 })
 
 export class SideMenuComponent {
-
+    public menuItems = routes
+        .map((route) => route.children ?? [])
+        .flat()
+        .filter((route) => route && route.path)
+        .filter((route) => !route.path?.includes(':'));
 }

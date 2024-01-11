c723d6818dbe4d03a37be2dadfffc68454b20750
AminEl93 <amin_11_iverson@hotmail.com>
2024-01-11 18:42:59 +0100

    Control Flow - @for y @switch

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A' | 'B' | 'C' | 'D';
+
 @Component({
     standalone: true,
     imports: [CommonModule],
@@ -10,7 +12,11 @@ import { Component, signal } from '@angular/core';
 export default class ControlFlowComponent {
 
     public showContent = signal(false);
+    public grade = signal<Grade>('A');
     
+    public frameworks = signal(['Angular', 'React', 'Vue', 'NextJS', 'Svelte', 'Django', 'Laravel']);
+    public frameworks2 = signal([]);
+
     public toggleContent() {
         this.showContent.update(value => !value);    
     }
